d218d1115e2b5e2337d1e6a55665d652d8ef063f
Tim Kapitza <monarch38108@gmail.com>
2026-01-17 11:48:03 +0100

    feat(models): add Note model with fillable and cast attributes

---
+++ app/Models/Note.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'is_important'
+    ];
+
+    protected $casts = [
+        'is_important' => 'boolean' // Weil bool in SQLite als 0 oder 1 gespeichert wird und ich im code aber bool verwende
+    ];
+}
