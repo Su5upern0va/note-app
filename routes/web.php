7ce5141c7c61f836c14bb655dfffc2bc7b19f280
Tim Kapitza <monarch38108@gmail.com>
2026-01-18 13:01:03 +0100

    refactor(routes): simplify note routes by using Livewire components (new livewire version I guess?)

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Notes\Index;
 use App\Livewire\Notes\Form;
 
-Route::get('/', function () {
-    return redirect()->route('notes.index');
-});
-
-Route::get('/notes', Index::class)->name('notes.index');
-
-Route::get('notes/create', function () {
-    return view('notes.create');
-})->name('notes.create');
-
-Route::get('notes/{noteId}/edit', function ($noteId) {
-    return view('notes.edit', ['noteId' => $noteId]);
-})->name('notes.edit');
+Route::get('/', Index::class)->name('notes.index');
+Route::get('/notes/create', Form::class)->name('notes.create');
+Route::get('/notes/{noteId}/edit', Form::class)->name('notes.edit');
