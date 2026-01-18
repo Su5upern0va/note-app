656b7fc442b95709a87c280e7ba647301b281f84
Tim Kapitza <monarch38108@gmail.com>
2026-01-18 13:35:37 +0100

    refactor(livewire): prioritize important notes in index query

--- app/Livewire/Notes/Index.php
+++ app/Livewire/Notes/Index.php
@@ -10,7 +10,7 @@ class Index extends Component
     public function render()
     {
         return view('livewire.notes.index', [
-            'notes' => Note::latest()->get()
+            'notes' => Note::orderBy('is_important', 'desc')->latest()->get()
         ]);
     }
 
