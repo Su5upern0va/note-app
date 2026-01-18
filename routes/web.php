dcf1a132c45f4464abea5bd46f41bf8a2b7fc113
Tim Kapitza <monarch38108@gmail.com>
2026-01-18 12:40:30 +0100

    feat(routes): add routes for notes index, create, and edit pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Notes\Index;
+use App\Livewire\Notes\Form;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('notes.index');
 });
+
+Route::get('/notes', Index::class)->name('notes.index');
+
+Route::get('notes/create', function () {
+    return view('notes.create');
+})->name('notes.create');
+
+Route::get('notes/{noteId}/edit', function ($noteId) {
+    return view('notes.edit', ['noteId' => $noteId]);
+})->name('notes.edit');
